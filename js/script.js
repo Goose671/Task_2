5edc7efe952499f11a0b1ffcbb47375d5c617766
Goose <Hus.O.Y@nmu.one>
2022-06-25 22:58:22 +0300

    feature - add Local Storage

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-const btn__add = document.getElementById("btn__add")
-btn__add.addEventListener('click',addTask)
+const btn__add = document.getElementById("btn__add");
+btn__add.addEventListener('click', addTask);
 
-let text = document.getElementById("input")
+let text = document.getElementById("input");
 
-let taskList = document.getElementById('task-list')
+let taskList = document.getElementById('task-list');
 
-function addTask(){
-     
+const tasks = [];
+
+ displayTasks();
+
+function addTask() {
+
     let task_div = document.createElement("div")
 
     task_div.classList.add("task")
+
+    // task_div.id = numberId++
 
     taskList.prepend(task_div)
 
@@ -19,7 +25,7 @@ function addTask(){
 
     task_p.classList.add("task__text")
 
-    task_p.innerHTML = text.value 
+    task_p.innerHTML = text.value
 
     task_div.append(task_p)
 
@@ -43,7 +49,7 @@ function addTask(){
 
     // создал внутри  button елемент img с классом "icon"
 
-     task_btn = document.createElement("button")
+    task_btn = document.createElement("button")
 
     task_btn.classList.add("btn__icon")
 
@@ -61,6 +67,81 @@ function addTask(){
 
     // создал внутри  button елемент img с классом "icon"
 
-    text.value = ""
+    const task = {
+        text: text.value
+    }
+    tasks.push(task)
+    localStorage.setItem("tasks", JSON.stringify(tasks))
 
+     text.value = ""
+
+};
+
+function displayTasks() {
+
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if (localStorage.getItem('tasks')) {
+
+        tasks.forEach(element => {
+            let task_div = document.createElement("div")
+
+            task_div.classList.add("task")
+
+            // task_div.id = numberId++
+
+            taskList.prepend(task_div)
+
+            // cоздал div с классом "task"
+
+            let task_p = document.createElement("p")
+
+            task_p.classList.add("task__text")
+
+            task_p.innerHTML = element.text
+
+            task_div.append(task_p)
+
+            // создал внутри div  елемент p с классом "task__text"
+
+            let task_btn = document.createElement("button")
+
+            task_btn.classList.add("btn__icon")
+
+            task_div.append(task_btn)
+
+            // создал внутри div елемент buttton с классом "btn__icon" 
+
+            let task_btn_pencil = document.createElement("img")
+
+            task_btn_pencil.classList.add("icon")
+
+            task_btn_pencil.src = "img/pencil-solid.svg"
+
+            task_btn.append(task_btn_pencil)
+
+            // создал внутри  button елемент img с классом "icon"
+
+            task_btn = document.createElement("button")
+
+            task_btn.classList.add("btn__icon")
+
+            task_div.append(task_btn)
+
+            // создал внутри div елемент buttton с классом "btn__icon" 
+
+            task_btn_pencil = document.createElement("img")
+
+            task_btn_pencil.classList.add("icon")
+
+            task_btn_pencil.src = "img/trash-can-solid.svg"
+
+            task_btn.append(task_btn_pencil)
+
+            // создал внутри  button елемент img с классом "icon"  
+        });
+            
+        
+
+    }
 }
